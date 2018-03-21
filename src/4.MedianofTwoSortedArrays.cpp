df60377b1fc142d04bd34e687e01b7ced0040547
Ming <wming9310@gmail.com>
2018-03-21 22:13:14 +0800

    rethinking p4

--- src/4.MedianofTwoSortedArrays.cpp
+++ src/4.MedianofTwoSortedArrays.cpp
@@ -1,47 +1,6 @@
 class Solution {
  public:
-    double solve(vector<int>& max, vector<int>& min){
-        int m, n;
-        int minlen, maxlen;
-        maxlen = max.size();
-        minlen = min.size();
-        m = maxlen /2;
-        n = minlen /2;
-        int jumpm = m / 2;
-        int jumpn = n / 2;
-        while (true){
-            cout<<"m"<<m<<n<<endl;
-            cout<<"ma"<<max[m]<<min[n]<<endl;
-            cout<<"j"<<jumpm<<jumpn<<endl;
-            if (max[m] == min[n]) return max[m];
-            if /*((m==0)&&(n=minlen-1)
-                ||*/((jumpn==0)&&(jumpm==0))/*)*/ {
-                if (m+n == (maxlen+minlen)/2) return min[n];
-                if (m+n+1 == (maxlen+minlen)/2) return max[m];
 
-                return (max[m]+min[n])/2.0;
-            }
-            if (max[m] > min[n]) {
-                if (m>0) {
-                    m -= jumpm;
-                    jumpm = jumpm / 2;
-                } else jumpm = 0;
-                if (n<minlen-1) {
-                    n += jumpn;
-                    jumpn = jumpn / 2;
-                } else jumpn = 0;
-            } else {
-                if (n>0) {
-                    n -= jumpn;
-                    jumpn = jumpn / 2;
-                } else jumpn = 0;
-                if (m<maxlen-1) {
-                    m += jumpm;
-                    jumpm = jumpm / 2;
-                } else jumpm = 0;
-            }
-        }
-    }
 
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 
