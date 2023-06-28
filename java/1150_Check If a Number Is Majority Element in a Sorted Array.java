f1ce26a4a56744462edb8e6bcf69f8f2ad4227e4
Ming91 <wming9310@gmail.com>
2023-06-28 14:50:57 -0400

    Update 1150_Check If a Number Is Majority Element in a Sorted Array.java

--- java/1150_Check If a Number Is Majority Element in a Sorted Array.java
+++ java/1150_Check If a Number Is Majority Element in a Sorted Array.java
@@ -1,14 +1,18 @@
 class Solution {
     private static int binarySearch(int[] arr, int l, int r, int x) {
-        if (r >= l) {
-            int mid = (r + l) / 2;
-            if (arr[mid] == x && (mid == 0 || arr[mid - 1] < x)) {
-                return mid;
-            }
+        int ans = -1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
             if (arr[mid] >= x) {
-                return binarySearch(arr, l, mid - 1, x);
+                r = mid - 1;
+                ans = mid;
+            } else {
+                l = mid + 1;
             }
-            return binarySearch(arr, mid + 1, r, x);
+        }
+        //System.out.println(l + "," + r);
+        if (ans >= 0 && arr[ans] == x) {
+            return ans;
         }
         return -1;
     }
@@ -18,7 +22,7 @@ class Solution {
         int compareBase = len / 2;
         
         int firstIdx = binarySearch(nums, 0, len - 1, target);
-        System.out.println(firstIdx);
+        //System.out.println(firstIdx);
         if (firstIdx < 0) {
             return false;
         }
@@ -28,3 +32,5 @@ class Solution {
         return true;
     }
 }
+
+// frequency count works here
