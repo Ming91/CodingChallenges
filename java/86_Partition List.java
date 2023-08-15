de0a055891af0b36e95928f33807296fe5e8c1e8
Ming91 <wming9310@gmail.com>
2023-08-14 20:17:22 -0400

    Create 86_Partition List.java
    Daily Challenge 08/14/2023

---
+++ java/86_Partition List.java
@@ -0,0 +1,45 @@
+// Daily Challenge 08/14/2023
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode less = new ListNode(-101);
+        ListNode more = new ListNode(-101);
+        ListNode lessHead = less;
+        ListNode moreHead = more;
+        while (head != null) {
+            if (head.val < x) {
+                less.next = head;
+                less = head;
+            } else {
+                more.next = head;
+                more = head;
+            }
+            head = head.next;
+        }
+        // if (less == lessHead) {
+        //     return moreHead.next;
+        // }
+        // if (more == moreHead) {
+        //     return lessHead.next;
+        // }
+
+        // if not assign null, there will be cycle
+        // [2, 4, 2], 3 -> more [4, 2]
+        more.next = null;
+        less.next = moreHead.next;
+        return lessHead.next;
+    }
+}
+// Linked List exp++
