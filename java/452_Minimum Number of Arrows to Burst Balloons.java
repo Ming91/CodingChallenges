f68de595bf2087b24979a58045e369360fa898ee
Ming91 <wming9310@gmail.com>
2023-09-06 14:57:02 -0400

    Create 452_Minimum Number of Arrows to Burst Balloons.java
    LeetCode 75 Intervals Q2

---
+++ java/452_Minimum Number of Arrows to Burst Balloons.java
@@ -0,0 +1,77 @@
+// LeetCode 75 Intervals Q2
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        // Sort Array by End
+        // Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+        // int ans = 1, shot = points[0][1];
+        // for (int i = 1; i < points.length; i++) {
+        //     if (shot < points[i][0]) {
+        //         shot = points[i][1];
+        //         ans++;
+        //     }
+        // }
+        long[] p = new long[points.length];
+        int n = 0;
+        for (int[] point : points) {
+            // if not add L at the end, when point[0] < 0, will effect point[1] sign to -1
+            p[n++] = (((long)point[1] << 32) | (point[0] & 0xFFFF_FFFFL));
+            // System.out.println((int)(p[n - 1] >> 32)+ "," + (int)(p[n - 1] & 0xFFFF_FFFFL));
+        }
+        // System.out.println((int)(0x1_FFFF_FFFFL & 0xFFFF_FFFFL));
+        Arrays.sort(p);
+        int ans = 1;
+        int shot = (int)(p[0] >> 32);
+        for (int i = 1; i < n; i++) {
+            if (shot < (int)(p[i] & 0xFFFF_FFFF)) {
+                shot = (int)(p[i] >> 32);
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
+// faster idea: combine two numbers and sort
+
+// correct idea:
+//  sort on end, shot on end
+
+// why can shor on start?
+//  Can shot, if inverse order start.
+//  The point is, shot on start too early
+
+// forgot this classic problem, 2nd time!!!
+// naive intuition...
+// sort on start, and some stupid modification
+
+// class Solution {
+//     public int findMinArrowShots(int[][] points) {
+
+//         // cause overflow when a-b too large (eg. max - min)
+//         // Arrays.sort(points, (a, b) -> {
+//         //     if (a[0] == b[0]) {
+//         //         return a[1] - b[1];
+//         //     }
+//         //     return a[0] - b[0];
+//         // });
+//         Arrays.sort(points, (a, b) -> {
+//             if (Integer.valueOf(a[0]).equals(b[0])) {
+//                 return Integer.compare(a[1], b[1]);
+//             }
+//             return Integer.compare(a[0], b[0]);
+//         });
+//         int ans = 1;
+//         int shot = points[0][1];
+//         for (int i = 0; i < points.length; i++) {
+//             int[] point = points[i];
+//             // System.out.println(shot + " " + point[0]);
+//             if (point[0] > shot) {
+//                 shot = point[1];
+//                 ans++;
+//             }
+//             if (point[1] < shot) {
+//                 shot = point[1];
+//             }
+//         }
+//         return ans;
+//     }
+// }
