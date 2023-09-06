f02170c5b9093670b81c5c787fdc6b90d83d9e55
Ming91 <wming9310@gmail.com>
2023-09-06 15:15:52 -0400

    Update 238_Product of Array Except Self.java
    Top Interview 150 Q13

--- java/238_Product of Array Except Self.java
+++ java/238_Product of Array Except Self.java
@@ -1,18 +1,40 @@
+// Top Interview 150 Q13
+// 08/28/2023 Impl
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int pre = 1;
-        int suff = 1;
         int[] ans = new int[n];
+        int prev = 1;
         ans[0] = 1;
         for (int i = 1; i < n; i++) {
-            pre *= nums[i - 1];
-            ans[i] = pre;
+            prev *= nums[i - 1];
+            ans[i] = prev;
         }
+        prev = 1;
         for (int i = n - 2; i >= 0; i--) {
-            suff *= nums[i + 1];
-            ans[i] *= suff;
+            prev *= nums[i + 1];
+            ans[i] *= prev;
         }
         return ans;
     }
 }
+
+
+// class Solution {
+//     public int[] productExceptSelf(int[] nums) {
+//         int n = nums.length;
+//         int pre = 1;
+//         int suff = 1;
+//         int[] ans = new int[n];
+//         ans[0] = 1;
+//         for (int i = 1; i < n; i++) {
+//             pre *= nums[i - 1];
+//             ans[i] = pre;
+//         }
+//         for (int i = n - 2; i >= 0; i--) {
+//             suff *= nums[i + 1];
+//             ans[i] *= suff;
+//         }
+//         return ans;
+//     }
+// }
