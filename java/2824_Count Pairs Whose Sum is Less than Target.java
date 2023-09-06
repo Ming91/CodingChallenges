f24fe36501acb8a09d134e2d40049d5d0dd46b28
Ming91 <wming9310@gmail.com>
2023-09-06 11:52:07 -0400

    Create 2824_Count Pairs Whose Sum is Less than Target.java
    Biweekly Contest 111 Q1

---
+++ java/2824_Count Pairs Whose Sum is Less than Target.java
@@ -0,0 +1,14 @@
+// Biweekly Contest 111 Q1
+class Solution {
+    public int countPairs(List<Integer> nums, int target) {
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums.get(i) + nums.get(j) < target) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
