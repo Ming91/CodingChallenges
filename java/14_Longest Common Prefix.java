afde75a33c634a78c658f62b0e662f63230643aa
Ming91 <wming9310@gmail.com>
2023-09-06 15:21:18 -0400

    Create 14_Longest Common Prefix.java
    Top Interview 150 Q20

---
+++ java/14_Longest Common Prefix.java
@@ -0,0 +1,20 @@
+// Top Interview 150 Q20
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (String s : strs) {
+                if (i >= s.length() || s.charAt(i) != c) {
+                    return sb.toString();
+                }
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
+// This is a vertical impl
+// can also using horizontal impl, scan prefix from [0], then substract length by 1 if not valid
+
+// Trie is also an option
