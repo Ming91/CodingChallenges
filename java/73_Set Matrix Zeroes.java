166fa016659e79075e6588fe2e148c1804fb50f5
Ming91 <wming9310@gmail.com>
2023-09-08 14:46:19 -0400

    Create 73_Set Matrix Zeroes.java
    Top Interview 150 Matrix Q4

---
+++ java/73_Set Matrix Zeroes.java
@@ -0,0 +1,50 @@
+// Top Interview 150 Matrix Q4
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean firstColZero = false;
+        boolean firstRowZero = false;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if (firstRowZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        return ;
+    }
+}
+
+// use first col and row as mark, but be careful of when the cell in it used as another mark
+// eg. [0, 2] = 0, if set[0, 0] to 0, it will effect [0, i].
+// so, leave first col and row alone.
