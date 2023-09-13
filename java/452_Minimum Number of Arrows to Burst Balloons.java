44f372fc155af50d01bdba3f96239d2adc5d45ee
Ming91 <wming9310@gmail.com>
2023-09-13 15:28:13 -0400

    Update 452_Minimum Number of Arrows to Burst Balloons.java
    Top Interview 150 Intervals Q4

--- java/452_Minimum Number of Arrows to Burst Balloons.java
+++ java/452_Minimum Number of Arrows to Burst Balloons.java
@@ -1,4 +1,49 @@
 // LeetCode 75 Intervals Q2
+// Top Interview 150 Intervals Q4
+
+// [TopInterview150] 09/13/2023 Impl Part 2
+// class Solution {
+//     public int findMinArrowShots(int[][] points) {
+//         int n = points.length;
+//         long[] joined = new long[n];
+//         int i = 0;
+//         for (int[] point : points) {
+//             joined[i++] = ((long)point[1] << 32) | (point[0] & 0xFFFF_FFFFL);
+//         }
+//         Arrays.sort(joined);
+//         int ans = 0;
+//         long x = Long.MIN_VALUE;
+//         for (long j : joined) {
+//             int start = (int)(j & (0xFFFF_FFFFL));
+//             int end = (int)((j & 0xFFFF_FFFF_0000_0000L) >> 32);
+//             // System.out.printf("%d, %d%n", start, end);
+//             if (start > x) {
+//                 ans++;
+//                 x = end;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+// [TopInterview150] 09/13/2023 Impl Part 1
+// class Solution {
+//     public int findMinArrowShots(int[][] points) {
+//         Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+//         int n = points.length;
+//         int ans = 1;
+//         int i = 1;
+//         int x = points[0][1];
+//         while (i < n) {
+//             if (points[i][0] > x) {
+//                 ans++;
+//                 x = points[i][1];
+//             }
+//             i++;
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int findMinArrowShots(int[][] points) {
         // Sort Array by End
@@ -75,3 +120,4 @@ class Solution {
 //         return ans;
 //     }
 // }
+
