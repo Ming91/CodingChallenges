9f8f07f37aae099469cf3c5ae3a2d50dac2fc48b
Ming91 <wming9310@gmail.com>
2023-09-14 02:15:53 -0400

    Update 332_Reconstruct Itinerary.java
    formatting java file

--- java/332_Reconstruct Itinerary.java
+++ java/332_Reconstruct Itinerary.java
@@ -1,23 +1,21 @@
 // Daily Question 09/14/2023
 class Solution {
-    int n;
-    boolean found;
-    void dfs(Map<String, Queue<String>> adjLists, 
-             List<String> ans, String curr) {
+    void dfs(Map<String, Queue<String>> adjLists,
+            List<String> ans, String curr) {
         Queue<String> neighbors = adjLists.get(curr);
         if (neighbors == null) {
             ans.add(curr);
-            return ;
+            return;
         }
         while (!neighbors.isEmpty()) {
             String neighbor = neighbors.poll();
             dfs(adjLists, ans, neighbor);
         }
         ans.add(curr);
-        return ;
+        return;
     }
+
     public List<String> findItinerary(List<List<String>> tickets) {
-        n = tickets.size();
         Map<String, Queue<String>> adjLists = new HashMap<>();
         for (List<String> ticket : tickets) {
             String from = ticket.get(0);
