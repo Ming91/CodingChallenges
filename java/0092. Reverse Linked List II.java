300e7fc3e29aef192db1680cdb8967fc0cd187b3
Ming91 <wming9310@gmail.com>
2023-09-20 13:39:33 -0400

    Update 0092. Reverse Linked List II.java
    Top Interview 150 LinkedList Q5

--- java/0092. Reverse Linked List II.java
+++ java/0092. Reverse Linked List II.java
@@ -1,4 +1,5 @@
 // Daily Problem 09/07/2023
+// Top Interview 150 LinkedList Q5
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -9,34 +10,69 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// Top Interview 150 09/20/2023 Impl
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
         if (left == right) {
             return head;
         }
-        ListNode curr = head;
         ListNode prev = null;
-        int count = 0;
-        while (count < left - 1) {
+        ListNode curr = head;
+        int idx = 1;
+        while (idx < left) {
             prev = curr;
             curr = curr.next;
-            count++;
+            idx++;
         }
-        ListNode leftPrev = prev;
-        ListNode end = curr;
-        while (count < right) {
-            ListNode temp = curr.next;
+        ListNode leftEnd = prev;
+        ListNode tail = curr;
+        while (idx <= right) {
+            ListNode next = curr.next;
             curr.next = prev;
             prev = curr;
-            curr = temp;
-            count++;
+            curr = next;
+            idx++;
         }
-        end.next = curr;
-        if (left == 1) {
-            return prev;
-        } else {
-            leftPrev.next = prev;
+        if (tail != null) {
+            tail.next = curr;
+        }
+        if (leftEnd != null) {
+            leftEnd.next = prev;
             return head;
+        } else {
+            return prev;
         }
     }
 }
+// Daily Problem 09/07/2023 Impl
+// class Solution {
+//     public ListNode reverseBetween(ListNode head, int left, int right) {
+//         if (left == right) {
+//             return head;
+//         }
+//         ListNode curr = head;
+//         ListNode prev = null;
+//         int count = 0;
+//         while (count < left - 1) {
+//             prev = curr;
+//             curr = curr.next;
+//             count++;
+//         }
+//         ListNode leftPrev = prev;
+//         ListNode end = curr;
+//         while (count < right) {
+//             ListNode temp = curr.next;
+//             curr.next = prev;
+//             prev = curr;
+//             curr = temp;
+//             count++;
+//         }
+//         end.next = curr;
+//         if (left == 1) {
+//             return prev;
+//         } else {
+//             leftPrev.next = prev;
+//             return head;
+//         }
+//     }
+// }
