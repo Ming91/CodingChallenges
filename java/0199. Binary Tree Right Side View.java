a8e175b1710dcd0853d5fff7bf6b7903668fe5f2
Ming91 <wming9310@gmail.com>
2023-09-27 12:47:20 -0400

    Update 0199. Binary Tree Right Side View.java
    Top Interview 150 Binary Tree BFS Q1

--- java/0199. Binary Tree Right Side View.java
+++ java/0199. Binary Tree Right Side View.java
@@ -1,3 +1,4 @@
+// Top Interview 150 Binary Tree BFS Q1
 // LeetCode 75 Binary Tree - BFS Q1
 /**
  * Definition for a binary tree node.
@@ -14,24 +15,51 @@
  *     }
  * }
  */
+// Top Interview 150 09/27/2023 Impl
 class Solution {
-    void dfs(List<Integer> ans, TreeNode curr, int len) {
-        if (curr == null) {
+    void dfs(TreeNode root, List<Integer> ans, int depth) {
+        if (root == null) {
             return ;
         }
-        if (ans.size() == len) {
-            ans.add(curr.val);
+        if (ans.size() == depth) {
+            ans.add(root.val);
         }
-        dfs(ans, curr.right, len + 1);
-        dfs(ans, curr.left, len + 1);
-        return ;
+        dfs(root.right, ans, depth + 1);
+        dfs(root.left, ans, depth + 1);
     }
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        dfs(ans, root, 0);
+        dfs(root, ans, 0);
         return ans;
     }
 }
+// Can be done with level order traversal.
+//  Just this about this perfect example:
+//    1
+//   / \
+//  2   3
+// /
+// 4
+
+// LeetCode 75 Impl
+// class Solution {
+//     void dfs(List<Integer> ans, TreeNode curr, int len) {
+//         if (curr == null) {
+//             return ;
+//         }
+//         if (ans.size() == len) {
+//             ans.add(curr.val);
+//         }
+//         dfs(ans, curr.right, len + 1);
+//         dfs(ans, curr.left, len + 1);
+//         return ;
+//     }
+//     public List<Integer> rightSideView(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         dfs(ans, root, 0);
+//         return ans;
+//     }
+// }
 // class Solution {
     
 //     public List<Integer> rightSideView(TreeNode root) {
