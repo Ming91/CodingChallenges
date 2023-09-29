fc0b776007b973f9648268c4af6d1d97a8d2009e
Ming91 <wming9310@gmail.com>
2023-09-29 14:03:42 -0400

    Update 0207. Course Schedule.java
    Top Interview 150 Graph General Q5

--- java/0207. Course Schedule.java
+++ java/0207. Course Schedule.java
@@ -1,42 +1,76 @@
+// Top Interview 150 Graph General Q5
+// Top Interview 150 09/29/23 Impl
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] inDegree = new int[numCourses];
-        Queue<Integer> q = new ArrayDeque<>();
-        List<Integer>[] child = new ArrayList[numCourses];
-        int visited = 0;
-        int numPre = prerequisites.length;
-        for (int i = 0; i < numCourses; i++) {
-            inDegree[i] = 0;
-            child[i] = new ArrayList<>();
+        int[] degree = new int[numCourses];
+        for (int[] prerequisite : prerequisites) {
+            degree[prerequisite[1]]++;
         }
-        for (int i = 0; i < numPre; i++) {
-            inDegree[prerequisites[i][1]]++;
-            child[prerequisites[i][0]].add(prerequisites[i][1]);
-        }
-        // don't forget this!!!
-        for (int i = 0; i < numCourses; i++) {
-            if (inDegree[i] == 0) {
-                q.offer(i);
-            }
-        }
-        while (!q.isEmpty()) {
-            Integer current = q.poll();
-            visited++;
-            for (Integer c : child[current]) {
-                inDegree[c]--;
-                if (inDegree[c] == 0) {
-                    q.offer(c);
+        int count = 0;
+        boolean updated = true;
+        int m = prerequisites.length;
+        boolean[] visited = new boolean[m];
+        while (updated) {
+            updated = false;
+            for (int i = 0; i < m; i++) {
+                if (visited[i] || degree[prerequisites[i][0]] > 0) {
+                    continue;
                 }
+                updated = true;
+                count++;
+                visited[i] = true;
+                degree[prerequisites[i][1]]--;
             }
         }
-        // add this to improve mem rank
-        //System.gc();
-        if (visited < numCourses) {
-            return false;
+        for (int i = 0; i < numCourses; i++) {
+            if (degree[i] > 0) {
+                return false;
+            }
         }
         return true;
     }
 }
+// Impl the beat 99% idea. Think it's just because the test case.
+
+// class Solution {
+//     public boolean canFinish(int numCourses, int[][] prerequisites) {
+//         int[] inDegree = new int[numCourses];
+//         Queue<Integer> q = new ArrayDeque<>();
+//         List<Integer>[] child = new ArrayList[numCourses];
+//         int visited = 0;
+//         int numPre = prerequisites.length;
+//         for (int i = 0; i < numCourses; i++) {
+//             inDegree[i] = 0;
+//             child[i] = new ArrayList<>();
+//         }
+//         for (int i = 0; i < numPre; i++) {
+//             inDegree[prerequisites[i][1]]++;
+//             child[prerequisites[i][0]].add(prerequisites[i][1]);
+//         }
+//         // don't forget this!!!
+//         for (int i = 0; i < numCourses; i++) {
+//             if (inDegree[i] == 0) {
+//                 q.offer(i);
+//             }
+//         }
+//         while (!q.isEmpty()) {
+//             Integer current = q.poll();
+//             visited++;
+//             for (Integer c : child[current]) {
+//                 inDegree[c]--;
+//                 if (inDegree[c] == 0) {
+//                     q.offer(c);
+//                 }
+//             }
+//         }
+//         // add this to improve mem rank
+//         //System.gc();
+//         if (visited < numCourses) {
+//             return false;
+//         }
+//         return true;
+//     }
+// }
 // 以上解法是topological sort using kahn's algo
 
 // beat 99% 的解法是不用明显的queue，记录每个course入度（有多少前置）
